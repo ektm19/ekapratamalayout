73059afecdf4212791eb3fc7486ad7425663cc33
ektm19 <33656586+ektm19@users.noreply.github.com>
2018-03-25 22:14:38 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TugasLayout from './src/comp/TugasLayout';
+
+export default class App extends React.Component {
+  render() {
+    return (
+    <TugasLayout />
+    );
+  }
+}
